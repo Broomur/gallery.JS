61ff0038a328b3d3d11419c3c5d2389e1e3afe8a
Simon <simonchauvel@gmail.com>
2022-08-16 15:54:08 +0200

    add a dynamic pagination

--- script2.js
+++ script2.js
@@ -1,8 +1,14 @@
+/* ***************************************
+***************  CLASSES  ****************
+*************************************** */
+
+
 class Page {
     constructor(url) {
         this.url = url;
     };
     displayImg(num) {
+        root.innerHTML = null;
         for(let i = 0; i < num; i++) {
             let picture = new Picture(i);
             picture.getImg(this.url);
@@ -24,40 +30,33 @@ class Picture {
         img.setAttribute("class", "col-12 col-md-4 mb-3")
         root.appendChild(img);
     };
-}
+};
+
+/* ***************************************
+*************** FUNCTIONS ****************
+*************************************** */
 
 function getURL(numPage) {
     return `https://picsum.photos/v2/list?page=${numPage}&limit=6`;
 }
 
-function previousBtn(numPage) {
-    let prevBtn = document.createElement("button");
-    prevBtn.innerHTML = "previous";
-    prevBtn.setAttribute("type", "button");
-    prevBtn.setAttribute("class", "btn btn-success m-3 position-absolute top-50 end-50");
-    btnContainer.appendChild(prevBtn);
-    prevBtn.addEventListener("click", () => {
-        root.innerHTML = null;
-        numPage--;
-        url = getURL(numPage);
-        page = new Page(url);
-        pictures = page.displayImg(6);
-    })
-};
-function nextBtn(numPage) {
-    let nexBtn = document.createElement("button");
-    nexBtn.innerHTML = "next";
-    nexBtn.setAttribute("type", "button");
-    nexBtn.setAttribute("class", "btn btn-success m-3 position-absolute top-50 start-50");
-    btnContainer.appendChild(nexBtn);
-    nexBtn.addEventListener("click", () => {
-        root.innerHTML = null;
-        numPage++;
-        url = getURL(numPage);
-        page = new Page(url);
-        page.displayImg(6);
-    })
+
+function pagination(numPage) {
+    numPage = parseInt(numPage);
+    if (numPage == 1) {
+        document.querySelector(".left").innerText = numPage;
+        document.querySelector(".central").innerText = numPage +1;
+        document.querySelector(".right").innerText = numPage +2;
+    } else {
+        document.querySelector(".left").innerText = numPage -1;
+        document.querySelector(".central").innerText = numPage;
+        document.querySelector(".right").innerText = numPage +1;
+    }
 }
+
+/* ***************************************
+*************** VARIABLES ****************
+*************************************** */
 
 let root = document.getElementById("root");
 let btnContainer = document.getElementById("btnContainer");
@@ -65,5 +64,38 @@ let numPage = 1;
 let url = getURL(numPage);
 let page = new Page(url);
 let pictures = page.displayImg(6);
-previousBtn(numPage);
-nextBtn(numPage);
+let paginNb = document.querySelectorAll(".paginNb");
+
+/* ***************************************
+*************** LISTENERS ****************
+*************************************** */
+
+document.getElementById("previous").addEventListener("click", () => {
+    if (numPage > 1) {
+        numPage--;
+        url = getURL(numPage);
+        page = new Page(url);
+        pictures = page.displayImg(6);
+        pagination(numPage);
+    }
+})
+
+document.getElementById("next").addEventListener("click", () => {
+    if (numPage < 100) {
+        numPage++;
+        url = getURL(numPage);
+        page = new Page(url);
+        pictures = page.displayImg(6);
+        pagination(numPage);
+    };
+});
+
+paginNb.forEach(item => {
+    item.addEventListener("click", () => {
+        numPage = item.innerText;
+        url = getURL(numPage);
+        page = new Page(url);
+        pictures = page.displayImg(6);
+        pagination(numPage);
+    });
+});
